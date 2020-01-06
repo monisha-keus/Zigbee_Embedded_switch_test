19122aaeb8df163cdd4aef6af3fc62e0a7a31cac
unknown <monisha@keus.in>
2020-01-06 15:03:18 +0530

    First commit to this repo

--- Components/keus/embeddedswitch/keus_task.h
+++ Components/keus/embeddedswitch/keus_task.h
@@ -13,76 +13,19 @@
 
 #define NVIC_MEMORY_POSITION 0x10
 
-// bool KeusThemeSwitchMiniMemoryInit(void);
-// bool KeusThemeSwitchMiniReadConfigDataIntoMemory(void);
-// bool KeusThemeSwitchMiniWriteConfigDataIntoMemory(void);
-
-
-#define ON      0x01
-#define OFF     0x00
-#define HIGH    255
-#define LOW     0x00
-
-#define EXECUTE_SWITCH_STATE     0X01
-#define CONFIGURE_SWITCH         0X02
-#define GET_SWITCH_STATE         0X04
-#define GET_CONFIG_STATE         0X08
-#define GET_BUTTON_STATE         0X10
-
-#define ONOFF                   1
-#define DIMMING                 2
-#define FAN_CONTROLLER          3
-
-#define SWITCH_1                1
-#define SWITCH_2                2
-#define SWITCH_3                3
-#define SWITCH_4                4
-
-
-
-// #define LED_CONTROL     1
-// #define SAVE_SCENE      2
-// #define EXECUTE_SCENE   3
-// #define DELETE_SCENE    4
-// #define GROUP_CONTROL   5
-
 #define LED_OFF         GPIO_HIGH
 #define LED_ON          GPIO_LOW
 
+#define LED_ACTIVE      GPIO_LOW
+#define LED_INACTIVE    GPIO_HIGH
+#define BUTTON_ACTIVE   GPIO_LOW
+#define BUTTON_INAVTIVE GPIO_HIGH
 
-//*****config data 
-struct CONFIG_INFO_t{
-    uint8   led;
-    uint8   valid_state;
-    uint8   config_id; 
-};
+#define KEUS_EMBEDDEDSWITCH_BUTTON_POLL_TIME 500
+#define KEUS_EMBEDDEDSWITCH_RESET_BTN_TIMEOUT 10000
 
 
-//Array of four config for four led
-// struct CONFIG_INFO_t{
-//    struct CONFIG   config_t[4];  
-// };
-
-//extern struct CONFIG_INFO_t config_data[4];
-
-extern void uart_send_ack(uint8 cmnd_id);
-extern void initUart(void);
-
-extern uint8 data_buff[20];
-extern uint8 uart_cmd_end;
-extern uint16 event_t;
-extern uint8 cmd_id;
-extern uint8 data_length;
 extern uint8 button_Pressed;
-extern uint8 switch_state_buffer;
-extern uint8 config_state_buffer;
-
-
-
-
-// extern bool KeusThemeSwitchMiniMemoryInit(void);
-// extern bool KeusThemeSwitchMiniReadConfigDataIntoMemory(void);
-// extern bool KeusThemeSwitchMiniWriteConfigDataIntoMemory(void);
 
 #endif
 
